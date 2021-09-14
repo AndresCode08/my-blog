37f3b0e7cca486b336cee5125501dde9fa3f5021
andresitodev <andresitodev@gmail.com>
2021-09-14 01:30:37 -0400

    first commit

---
+++ src/posts/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, PostView, Like, Comment
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .forms import PostForm
+
+class PostListView(ListView):
+    model = Post
+
+class PostDetailView(DetailView):
+    form_class = PostForm 
+    model = Post
+
+class PostCreateView(CreateView):
+    form_class = PostForm
+    model = Post
+    success_url = '/'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'view_type': 'create'
+        })
+        return context
+    
+    
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+class PostUpdateView(UpdateView):
+    form_class = PostForm
+    model = Post
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'view_type': 'update'
+        })
+        return context
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/'
+
+
+def like(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    like_qs = Like.objects.filter(user=request.user, post=post)
+    if like_qs.exists():
+        like_qs[0].delete()
+        return redirect('detail', slug=slug)
+    Like.objects.create(user=request.user, post=post)
+    return redirect('detail', slug=slug)
