d75fe0f23861877e74962ea5adc22418db48f85e
andresitodev <andresitodev@gmail.com>
2021-09-22 00:59:12 -0400

    new-commit

--- blog/settings.py
+++ blog/settings.py
@@ -1,20 +1,19 @@
 from pathlib import Path
 import os
+import environ 
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%8!x4do8m7%+pv6+k@1uvz0eo%d&q=+aqgyy&^rz4!%g4bxu(+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['https://my-blog-py.herokuapp.com/',]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
